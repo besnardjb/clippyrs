1b196aa0a264402c738718e1543b96467c58167e
BESNARD Jean-Baptiste <jbbesnard@paratools.com>
2024-07-30 11:09:13 +0200

    Clipboard initial support and add single run mode

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use anyhow::Result;
+use copypasta_ext::prelude::*;
+use copypasta_ext::x11_bin::ClipboardContext;
+use log::{error, info};
 use ollama::Ollama;
 use std::io::stdout;
 use std::io::{self, Write};
@@ -64,6 +67,32 @@ fn view_resp(skin: MadSkin, md: String) -> Result<(), Error> {
     Ok(())
 }
 
+fn prompt_unfold_vars(prompt: String) -> Result<String> {
+    let mut ret = prompt;
+
+    /* Input clipboard */
+    if ret.contains("::CL::") {
+        if let Ok(mut ctx) = ClipboardContext::new() {
+            let clipboard_data = ctx.get_contents().unwrap_or("".to_string());
+            ret = ret.replace("::CL::", &clipboard_data);
+        }
+    }
+
+    Ok(ret)
+}
+
+fn store_in_clipboard(response: String) {
+    if let Ok(mut ctx) = ClipboardContext::new() {
+        if let Err(e) = ctx.set_contents(response) {
+            error!("Failed to write content to clipboard: {}", e);
+        } else {
+            info!("Did set output data to clipboard");
+        }
+    } else {
+        error!("No ctx");
+    }
+}
+
 #[derive(Parser, Debug)]
 struct Args {
     /// Model to be used
@@ -75,6 +104,72 @@ struct Args {
     /// List available models
     #[clap(long, short, action)]
     list_models: bool,
+
+    /// Store response to clipboard
+    #[clap(long, short, action)]
+    store_in_clipboard: bool,
+
+    /// Optionnal Prompt
+    #[clap(last = true, allow_hyphen_values = true)]
+    prompt: Option<Vec<String>>,
+}
+
+async fn interactive(ollama: &Ollama, args: &Args, skin: &MadSkin) -> Result<()> {
+    let mut chat = ollama.context_new()?;
+
+    user_prompt();
+
+    for line in std::io::stdin().lines() {
+        let mut line = prompt_unfold_vars(line.unwrap())?;
+
+        let domd = if line.starts_with('!') {
+            line = line[1..].to_string();
+            true
+        } else {
+            false
+        };
+
+        assistant_prompt();
+
+        ollama.chat(line.as_str(), &mut chat).await?;
+
+        if let Some(resp) = chat.response() {
+            if domd || args.force_md {
+                let _ = view_resp(skin.clone(), resp.clone());
+            }
+
+            if args.store_in_clipboard {
+                store_in_clipboard(resp);
+            }
+        }
+
+        user_prompt()
+    }
+
+    Ok(())
+}
+
+async fn single(
+    ollama: &Ollama,
+    prompt: String,
+    args: &Args,
+    skin: &MadSkin,
+) -> Result<Option<String>> {
+    let mut chat = ollama.context_new()?;
+    let prompt = prompt_unfold_vars(prompt)?;
+    ollama.chat(prompt.as_str(), &mut chat).await?;
+
+    if let Some(response) = chat.response() {
+        if args.force_md {
+            let _ = view_resp(skin.clone(), response.clone());
+        }
+
+        if args.store_in_clipboard {
+            store_in_clipboard(response);
+        }
+    }
+
+    Ok(chat.response())
 }
 
 #[tokio::main]
@@ -96,36 +191,17 @@ async fn main() -> Result<()> {
         return Ok(());
     }
 
-    if let Some(model) = args.model {
+    if let Some(model) = &args.model {
         ollama.set_model(model.as_str())?;
     }
 
-    let mut chat = ollama.context_new()?;
-
-    user_prompt();
-
-    for line in std::io::stdin().lines() {
-        let mut line = line.unwrap();
-
-        let domd = if line.starts_with('!') {
-            line = line[1..].to_string();
-            true
-        } else {
-            false
-        };
-
-        assistant_prompt();
-
-        ollama.chat(line.as_str(), &mut chat).await?;
-
-        if let Some(resp) = chat.response() {
-            if domd || args.force_md {
-                let _ = view_resp(skin.clone(), resp);
-            }
-        }
-
-        user_prompt()
+    if let Some(prompt) = &args.prompt {
+        let pr = prompt.join(" ");
+        single(&ollama, pr, &args, &skin).await?;
+        return Ok(());
     }
+
+    interactive(&ollama, &args, &skin).await?;
 
     Ok(())
 }
